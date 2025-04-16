4dee68a5fe1f810d0a3532cdf9eca60449df2e03
Tim2othy <150283209+Tim2othy@users.noreply.github.com>
2025-04-16 20:46:32 +0200

    lint plus print

--- mnist_nn/start_transformer.py
+++ mnist_nn/start_transformer.py
@@ -1,15 +1,17 @@
 from activation_Layer import ActivationLayer
+from attention import AttentionLayer
 from layers import FCLayer, SoftmaxLayer, relu, relu_prime
 from losses import cross_entropy, cross_entropy_prime
 from mnist_loader import finish_up_data
 from network import Network
 
-from mnist_nn.attention import AttentionLayer
+print("Starting Transformer MNIST")
 
 # Create the network
 net = Network(cross_entropy, cross_entropy_prime)
 net.add(FCLayer(28 * 28, 100))  # input_shape=(1, 28*28)    ;   output_shape=(1, 100)
 net.add(ActivationLayer(relu, relu_prime))
+print("before attention")
 net.add(AttentionLayer(100, 50))  # input_shape=(1, 100)      ;   output_shape=(1, 50)
 net.add(ActivationLayer(relu, relu_prime))
 net.add(FCLayer(50, 10))  # input_shape=(1, 50)       ;   output_shape=(1, 10)
